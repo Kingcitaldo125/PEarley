daff1b6ad4a9d8ef0fe2e715ec4f79afabea402f
paula <paularelt@gmail.com>
2021-05-07 14:29:38 -0400

    Remove gross debug code; Add comments

--- earley.py
+++ earley.py
@@ -34,7 +34,6 @@ def swap_around_dot(state, dot):
 	global nonterminals
 	global parts_of_speech
 	lax = list(state[1])
-	#print("swap_around_dot lax",lax)
 	swap_around_terminal = False
 	idx = lax.index(dot)
 	x=lax[idx + 1]
@@ -59,8 +58,6 @@ def get_next_element(state, dot):
 	global nonterminals
 	global parts_of_speech
 	state_payload = state[1]
-	#print("get_next_element state_payload", state_payload])
-	#print("state_payload.index(dot)", state_payload.index(dot))
 	dotIdx = state_payload.index(dot)
 	if dotIdx == len(state_payload)-1:
 		#print("CANNOT GET NEXT STATE ELEM")
@@ -68,11 +65,12 @@ def get_next_element(state, dot):
 
 	nxt_elem = None
 	is_terminal = False
+
 	# Logic here is in place to know if the next element that we're trying
 	# to retrieve, relative to the '•', is either a terminal or nonterminal
 	# symbol
 	nxt_item = state_payload[state_payload.index(dot)+1]
-	#print("nxt_item",nxt_item)
+
 	if nxt_item not in nonterminals and nxt_item not in parts_of_speech:
 		is_terminal = True
 
@@ -81,7 +79,6 @@ def get_next_element(state, dot):
 	else:
 		nxt_elem = state_payload[state_payload.index(dot)+1]
 
-	#print("nxt_elem:",nxt_elem)
 	return nxt_elem
 
 def state_print(S):
@@ -92,12 +89,10 @@ def state_print(S):
 def predict(S, k, element, words, grammar, do_state_print=False):
 	global dot
 	global prod_split
-	#print(f"PREDICTING k={k}")
-	#print(f"{szero} in nonterminals")
+
 	added = False
 	prod = element
 	gprod = grammar[prod]
-	#print("prod",prod,"->","gprod",gprod)
 
 	# continue to process each of the processed productions
 	# split out each nonterminal's RHS, and then split on the '|'(pipe)
@@ -107,7 +102,6 @@ def predict(S, k, element, words, grammar, do_state_print=False):
 		dProd = dot + prodd
 		n_state = (prod, "".join(dProd.split(" ")), k)
 		if not n_state in S[k]:
-			#print("Adding production",n_state)
 			S[k].append(n_state)
 			added = True
 
@@ -120,21 +114,20 @@ def predict(S, k, element, words, grammar, do_state_print=False):
 def scan(S, k, state, words, do_state_print=False):
 	global dot
 	global parts_of_speech
-	#print(f"SCANNING; k={k}")
-	#print("words[k]", words[k])
-	#print("state[1]", state[1])
+
 	nxt_elem_scanner = get_next_element(state, dot)
-	#print("nxt_elem_scanner", nxt_elem_scanner)
+
 	added = False
 	if k > len(words)-1:
 		return added
+
 	# Check to see if the terminal symbol is a member of the language
 	# If we've either gone 'over the deep end(k is out of range)', or we're in range,
 	# but the word is not a part of speech, then we can be reasonably certain that
 	# the input is not within the language
 	if words[k] in parts_of_speech and (nxt_elem_scanner == words[k] or nxt_elem_scanner == 'number'):
 		n_lax = swap_around_dot(state, dot)
-		#print("Adding Scanner", n_lax, "to set", k)
+
 		nitem = (state[0], n_lax, state[2])
 		if not nitem in S[k+1]:
 			S[k+1].append(nitem)
@@ -148,10 +141,11 @@ def scan(S, k, state, words, do_state_print=False):
 
 def complete(S, k, state, do_state_print=False):
 	global dot
-	#print(f"COMPLETING; k={k}")
+
 	x = state[2]
 	scopy = S[x].copy()
 	added = False
+
 	# Go through the list of copies and try to find
 	# a collection of existing nonterminals.
 	# This should be a reasonably good way of indicating
@@ -160,10 +154,10 @@ def complete(S, k, state, do_state_print=False):
 	# as it relates to S[k]
 	for ss in scopy:
 		ss_sym = "".join(ss[1].split(dot))
-		#print("ss_sym:",ss_sym)
+
 		if ss_sym[0] in nonterminals:
 			n_lax = swap_around_dot(ss, dot)
-			#print("Adding Completer", n_lax, "to set", k)
+
 			# Assemble the new state, as it relates to 'ss'
 			# and throw it in the associated set in the main container
 			nitem = (ss[0], n_lax, ss[2])
@@ -178,6 +172,10 @@ def complete(S, k, state, do_state_print=False):
 
 
 def earley_parse(words, grammar, do_state_print=False):
+	# Transduce the input, words, against the Earley algorithm's technique of
+	# predicting, scanning and completing.
+	# Return a boolean telling the caller whether or not the input, words, is 'in'
+	# the language described by the CFG(grammar)
 	global dot
 	global nonterminals
 	global prod_split
@@ -189,48 +187,35 @@ def earley_parse(words, grammar, do_state_print=False):
 	S[0].append(("P",dot+grammar["P"],0))
 	done = False
 	for k in range(len(words)+1):
-		#print(f"k loop {k}")
-		#print("S",S[k])
 		added = True
 		if done:
 			break
+
 		# Since the original pseudocode on Wikipedia is a bit lacking for the
 		# original portion of the state sequence,
 		# 'bootstrap' the algorithm foward by having it only move to the next state/set
 		# when we did not add anything to 'S'
-		while added and not done:
+		while added:
 			added = False
 			for state in S[k].copy():
 				if expected in S[len(words)]:
 					done = True
 					break
-				#print("state loop")
-				#print("state",state)
 				if not is_finished(state, dot):
 					nxt_elem = get_next_element(state, dot)
 					if is_nonterminal(nxt_elem): # predict
-						#print("Predicting")
-						added = predict(S, k, nxt_elem, words, grammar)
-						#added = predict(S, k, nxt_elem, words, grammar, do_state_print = do_state_print)
-						#print("Predicted", added)
+						added = predict(S, k, nxt_elem, words, grammar, do_state_print = do_state_print)
 					else: # scanner(terminal)
-						#print("Scanning")
-						added = scan(S, k, state, words)
-						#added = scan(S, k, state, words, do_state_print = do_state_print)
-						#print("Scanned", k)
+						added = scan(S, k, state, words, do_state_print = do_state_print)
 				else: # we should be finished with a particular production
-					#print("Completing")
-					#added = complete(S, k, state)
 					added = complete(S, k, state, do_state_print = do_state_print)
-					#print("Completed", k)
-			#print("")
-			#print("")
-			#print("")
 
 	return expected in S[len(words)]
 
 
 def load_grammar(location):
+	# Load the grammar.txt from the disk;
+	# return the grammar as a string
 	g=""
 	with open(location, "r") as f:
 		for ff in f:
@@ -239,16 +224,19 @@ def load_grammar(location):
 
 
 def process_grammar(grammar):
+	# Take the grammar that was loaded from 'load_grammar'
+	# and load it into a dictionary
+	# This programatically accounts for each of the nonterminals
+	# in the CFG, and what they map to
 	gmap = {}
 	glist = grammar.split("\n")
 	# regex to tokenize the number to a lexeme
 	number_regex = re.compile(r"(\d\s[|]\s)+\d",re.M | re.I)
 	for g in glist:
-		#print("g",g)
 		x = g.split(" -> ")
-		#print(x)
 		lhs = x[0]
 		rhs = x[1]
+
 		if re.match(number_regex, rhs):
 			gmap[lhs] = "number"
 			continue
@@ -257,10 +245,9 @@ def process_grammar(grammar):
 
 
 def main():
-	grammar = load_grammar(".\\grammar.txt")
-	#print("Grammar:",grammar)
-	pgrammar = process_grammar(grammar)
-	#print("Processed Grammar:", pgrammar)
+	pgrammar = process_grammar(load_grammar(".\\grammar.txt"))
+	
+	# TBD: Move to unit tests
 	print(earley_parse("2+3*4", pgrammar)) # True
 	print(earley_parse("1", pgrammar)) # True
 	print(earley_parse("1+", pgrammar)) # False
